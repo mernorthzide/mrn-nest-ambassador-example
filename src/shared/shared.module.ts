ee1c26045dfbd731855db20a6e920aabad20ebf9
mernorthzide <mern_k@hotmail.com>
2023-08-17 11:50:46 +0700

    Add cache and redis

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,11 +1,19 @@
+import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import * as redisStore from 'cache-manager-redis-store';
+import type { RedisClientOptions } from 'redis';
 
 @Module({
   imports: [
     JwtModule.register({
       secret: 'secret',
       signOptions: { expiresIn: '1d' },
+    }),
+    CacheModule.register<RedisClientOptions>({
+      store: redisStore,
+      host: 'redis',
+      port: 6379,
     }),
   ],
   exports: [JwtModule],
